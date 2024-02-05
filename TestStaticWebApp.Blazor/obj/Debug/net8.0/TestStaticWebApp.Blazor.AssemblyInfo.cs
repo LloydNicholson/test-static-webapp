27f3f0f280e705fb825c1497c55a9b2fb4636f37
Lloyd <lloyd@stackworx.io>
2024-02-05 09:01:38 +0200

    fix: locations

--- TestStaticWebApp.Blazor/obj/Debug/net8.0/TestStaticWebApp.Blazor.AssemblyInfo.cs
+++ TestStaticWebApp.Blazor/obj/Debug/net8.0/TestStaticWebApp.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestStaticWebApp.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b044ff9a3c046a0f469faa97223c87a13960a35f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784970e4b6bbe0b5dae391e2a2c732844dbfebad")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestStaticWebApp.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestStaticWebApp.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
